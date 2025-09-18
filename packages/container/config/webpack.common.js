d22f864136d74f18ec8cb2e26293ecf260448db0
juan.m. <juan@example.com>
2025-09-18 14:17:54 +0200

    sintax error in webpack config fixed. trying deploy

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,21 +1,23 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-react','@babel/preset-env'],
-                        plugins: ['@babel/plugin-transform-runtime']
-                    }
-                }
-            },
-            new HtmlWebpackPlugin({
-                template: './public/index.html',
-            })
-        ]
-    }
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: [
+              "@babel/plugin-transform-runtime",
+              new HtmlWebpackPlugin({
+                template: "./public/index.html",
+              }),
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
